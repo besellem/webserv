74003b44d9117443f02adba69f904a3d4642cda2
Benjamin Sellem <besellem@student.42.fr>
2021-10-18 14:36:02 +0200

    init

---
+++ incs/webserv.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   webserv.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: besellem <besellem@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/18 14:22:05 by besellem          #+#    #+#             */
+/*   Updated: 2021/10/18 14:30:52 by besellem         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEBSERV_HPP
+# define WEBSERV_HPP
+
+# include <arpa/inet.h>
+# include <sys/select.h>
+# include <sys/types.h>
+# include <sys/event.h>
+# include <sys/time.h>
+# include <sys/socket.h>
+# include <sys/types.h>
+# include <fcntl.h>
+
+# include <iostream>
+# include <cstring>
+
+#endif /* !defined(WEBSERV_HPP) */
