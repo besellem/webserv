f14723e037058991e43fa2dda3f57f331ca771b8
Benjamin Sellem <besellem@student.42.fr>
2021-10-20 15:58:09 +0200

    up

--- incs/defs.hpp
+++ incs/defs.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   defs.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: besellem <besellem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 15:18:09 by besellem          #+#    #+#             */
-/*   Updated: 2021/10/19 14:36:21 by kaye             ###   ########.fr       */
+/*   Updated: 2021/10/20 15:57:37 by besellem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,29 @@
 # define DEFS_HPP
 
 /** @brief Anscii code */
-# define S_NONE "\033[0m"
-# define S_BLACK "\033[1;30m"
-# define S_RED "\033[1;31m"
-# define S_GREEN "\033[1;32m"
-# define S_YELLOW "\033[1;33m"
-# define S_BLUE "\033[1;34m"
-# define S_PURPLE "\033[1;35m"
-# define S_CYAN "\033[1;36m"
+# define S_NONE    "\033[0m"
+# define S_BLACK   "\033[1;30m"
+# define S_RED     "\033[1;31m"
+# define S_GREEN   "\033[1;32m"
+# define S_YELLOW  "\033[1;33m"
+# define S_BLUE    "\033[1;34m"
+# define S_PURPLE  "\033[1;35m"
+# define S_CYAN    "\033[1;36m"
 # define S_CLRLINE "\033[K\r"
 
 /** @brief namespace define */
 # define _BEGIN_NS_WEBSERV namespace webserv {
-# define _END_NS_WEBSERV }
+# define _END_NS_WEBSERV   }
+
+# define GET	(1L << 0)
+# define POST	(1L << 1)
+# define DELETE	(1L << 2)
+
 
 # define DEFAULT_CONFIG_FILE  "./config_files/default.conf"
 
 /** @brief epoll */
-# define kReadEvent 1
+# define kReadEvent  1
 # define kWriteEvent 2
 
 #endif /* !defined(DEFS_HPP) */
