e27520619733b26e6e73ce5623c2d8dc5a023018
adbenoit-9 <adeline.benoit@hotmail.com>
2021-11-02 12:45:52 +0100

    cgi post ok

--- test.php
+++
@@ -1,8 +0,0 @@
-<html>
- <head>
-  <title>Test PHP</title>
- </head>
- <body>
- <?php echo "CGI TEST OK !<p>"; ?>
-</body>
-</html>
--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-for i in range(100):
-	os.system(f"curl localhost:{int(sys.argv[1])} &");
-	# os.system(f"curl localhost:{int(sys.argv[1])}/index.html &");
-	# print(i)
